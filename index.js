984f12cc250cc5ab20aa8edacd56db64b3149016
Akshita Bhardwaj <akshita.bhardwaj2006@gmail.com>
2026-04-16 14:27:07 +0530

    changed file names

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const { writer } = require("repl");
 const { v4 : uuidv4 }= require ('uuid');
 
 app.set("view engine","ejs");
@@ -19,11 +20,11 @@ app.listen(port,()=>{
 
 app.get("/blogs",(req,res)=>{
     let recentBlogs = Blogs.slice(-6).reverse();
-    res.render("index.ejs",{recentBlogs});
+    res.render("Home.ejs",{recentBlogs});
 });
 
 app.get("/blogs/new",(req,res)=>{
-    res.render("write.ejs");
+    res.render("postBlog.ejs");
 });
 
 app.post("/blogs",(req,res)=>{
@@ -33,12 +34,15 @@ app.post("/blogs",(req,res)=>{
     res.redirect("/blogs");
 });
 app.get("/blogs/search",(req,res)=>{
-    let query = req.query.q.toLowerCase();
-    res.render("search.ejs");
+    let search = req.query.q;
+    const query=search.toLowerCase();
+    const blog= Blogs.find(b => writer === b.query);
+    console.log(blog);
+    
 
 });
 app.get("/blogs/:id",(req,res)=>{
     let {id}=req.params;
     let blog=Blogs.find((b)=>id===b.id);
-    res.render("see.ejs",{blog});
+    res.render("inDetail.ejs",{blog});
 });
